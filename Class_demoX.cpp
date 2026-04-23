96cff54653fead8d5acac3813d1fb4ab2f87c477
Sanika Kolhe <sanikakolhe127@gmail.com>
2026-04-24 00:41:18 +0530

    Add files via upload

---
+++ Class_demoX.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Demo
+{
+    public :
+        int i;                                //characteristics
+        float f;                              //characteristics
+
+        void fun()                            //Behaviour
+        {
+            cout<<"Inside fun \n";
+        }
+};
+
+int main()
+{
+    Demo dobj;
+
+    cout<<sizeof(dobj)<<"\n";                       //8
+
+    
+    return 0;
+}
