0dd1b912dbd11b552415b27b625b76557c8be2a0
Sanika Kolhe <sanikakolhe127@gmail.com>
2026-05-07 09:57:01 +0530

    Add files via upload

---
+++ DemoInterfaceMethod.java
@@ -0,0 +1,22 @@
+interface circle 
+{
+    //Characteristics (public static final)
+    float pi = 3.14f;  
+
+    //Behaviours (pub;ic Abstract)
+    float Araa(float Radius);
+    float Circumference(float Radius);
+
+}
+
+class Marvellous implements circle
+{
+    
+}
+public class DemoInterfaceMethod
+{
+    public static void main(String A[])
+    {
+       Marvellous mobj = new Marvellous();
+    }
+}
